a97324c5bbcdee22350b5fd57e979cd0c0c985fd
Michel Corne <mcorne@yahoo.com>
2019-12-16 16:33:37 +0100

    added user

--- app.py
+++ app.py
@@ -2,5 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.sqlite3"
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+db.drop_all()
+db.create_all()
+
+admin = User(username="admin", email="admin@example.com")
+guest = User(username="guest", email="guest@example.com")
+db.session.add(admin)
+db.session.add(guest)
+db.session.commit()
